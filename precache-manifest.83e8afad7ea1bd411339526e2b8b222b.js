00e93d0d4e5fa63541fe123700074a819eff64d2
tayfun akgün <tayfun_akgun@windowslive.com>
2021-02-05 22:06:21 +0300

    Updates

---
+++ precache-manifest.83e8afad7ea1bd411339526e2b8b222b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ee157bb01ba9173b8c80213560b31c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1a0c419acc08c1f88de",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "8a62ac29609a6c5cf7fd",
+    "url": "./static/js/2.a6b0fe2e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.a6b0fe2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a0c419acc08c1f88de",
+    "url": "./static/js/main.afe62026.chunk.js"
+  },
+  {
+    "revision": "47acdfc91bea7adf1a7c",
+    "url": "./static/js/runtime-main.bef1b1f6.js"
+  }
+]);
